e7dfe1d1d290433ec234db146e472c73828cbe18
Ochirov Aiur <ochirovaur2@mail.ru>
2020-01-06 21:12:16 +0800

    first commit

---
+++ src/js/index.js
@@ -0,0 +1,33 @@
+// import jQuery from "jquery";
+// import popper from "popper.js";
+// import bootstrap from "bootstrap";
+
+// import 'core-js/stable';
+// import 'regenerator-runtime/runtime';
+
+// import num from "./test";
+
+
+// let hello = 10;
+
+// const arr = ()=> {
+// 	console.log("es6");
+// }
+// console.log(num);
+// console.log(`${hello} it is ${num}`);
+// jQuery(function() {
+//   jQuery("body").css("color", "red");
+// });
+
+// var allo = new Map();
+// var ballo = Promise.resolve();
+
+// Object.assign({}, {1:1});
+
+// console.log("foobar".includes("foo"));
+
+// let sss = ["hello", 2]; // let
+
+//     let [str, times] = sss; // деструктуризация
+
+//     alert( str.repeat(times) ); // hellohello, метод repeat
